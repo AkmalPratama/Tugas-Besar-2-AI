d8f003f417e1061c54d963322d3fd22ecc19a172
Zacki Zulfikar Fauzi <zackizf1204@gmail.com>
2017-11-28 11:49:34 +0700

    Website jadi

--- BackEnd.py
+++ BackEnd.py
@@ -14,6 +14,31 @@ def result():
   data=pd.read_csv("D:/CencusIncome.data.txt", names=attributeName)
 	# GANTI PATHNYA ^^^^^^^^^^^^^^^^^^^^^
 
+  capitalgainmean = data["capital-gain"].mean()
+  capitallossmean = data["capital-loss"].mean()
+  hoursmean = data["hours-per-week"].mean()
+  workclassmode = data["workclass"].mode()[0]
+  educationmode = data["education"].mode()[0]
+  maritalmode = data["marital-status"].mode()[0]
+  occupationmode = data["occupation"].mode()[0]
+  relationshipmode = data["relationship"].mode()[0]
+  racemode = data["race"].mode()[0]
+  sexmode = data["sex"].mode()[0]
+
+  data["capital-gain"] = data[["capital-gain"]].replace(' ?', data["capital-gain"].mean())
+  data["capital-loss"] = data[["capital-loss"]].replace(' ?', data["capital-loss"].mean())
+  data["hours-per-week"] = data[["hours-per-week"]].replace(' ? ', data["hours-per-week"].mean())
+
+  data["workclass"] = data[["workclass"]].replace(' ?', " " + data["workclass"].mode()[0])
+  data["education"] = data[["education"]].replace(' ?', " " + data["education"].mode()[0])
+  data["marital-status"] = data[["marital-status"]].replace(' ?', " " + data["marital-status"].mode()[0])
+  data["occupation"] = data[["occupation"]].replace(' ?', " " + data["occupation"].mode()[0])
+  data["relationship"] = data[["relationship"]].replace(' ?', " " + data["relationship"].mode()[0])
+  data["race"] = data[["race"]].replace(' ?', " " + data["race"].mode()[0])
+  data["sex"] = data[["sex"]].replace(' ?', " " + data["sex"].mode()[0])
+
+
+
   cidata = data.as_matrix()
 
   le1 = preprocessing.LabelEncoder()
@@ -62,22 +87,31 @@ def result():
   list(le14.classes_)
   cidata[:,14] = le14.transform(cidata[:,14])
 
-  age = ' '+request.form['age']
   workclass = ' '+request.form['workclass']
-  fnlwgt = ' '+request.form['fnlwgt']
   education = ' '+request.form['education']
-  educationnum = ' '+request.form['education-num']
   maritalstatus = ' '+request.form['marital-status']
   occupation = ' '+request.form['occupation']
   relationship = ' '+request.form['relationship']
   race = ' '+request.form['race']
   sex = ' '+request.form['sex']
-  capitalgain = ' '+request.form['capital-gain']
-  capitalloss = ' '+request.form['capital-loss']
-  hoursperweek = ' '+request.form['hours-per-week']
-  nativecountry = ' '+request.form['native-country']
+  print("capital gain =" + request.form['capital-gain'] + "....")
+  if request.form['capital-gain']=="":
+    capitalgain = ' '+str(capitalgainmean)
+  else:
+    capitalgain = ' '+request.form['capital-gain']
 
-  appended = age+','+workclass+','+fnlwgt+','+education+','+educationnum+','+maritalstatus+','+occupation+','+relationship+','+race+','+sex+','+capitalgain+','+capitalloss+','+hoursperweek+','+nativecountry
+  if request.form['capital-loss']=="":
+    capitalloss = ' '+str(capitallossmean)
+  else:
+    capitalloss = ' '+request.form['capital-loss']
+
+  if request.form['hours-per-week']=="":
+    hoursperweek = ' '+str(hoursmean)
+  else:
+    hoursperweek = ' '+request.form['hours-per-week']
+
+  
+  appended = workclass+','+education+','+maritalstatus+','+occupation+','+relationship+','+race+','+sex+','+capitalgain+','+capitalloss+','+hoursperweek
 
   import sys
   if sys.version_info[0] < 3:
@@ -85,24 +119,32 @@ def result():
   else:
     from io import StringIO
   TESTDATA=StringIO(appended)
-  attributeName = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation","relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country"]
-  df = pd.read_csv(TESTDATA, names=attributeName)
-  cidata = df.as_matrix()
-  cidata[:,1] = le1.transform(cidata[:,1])
+  attributeName = ["workclass", "education", "marital-status", "occupation","relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week"]
+  data = pd.read_csv(TESTDATA, names=attributeName)
 
-  cidata[:,3] = le3.transform(cidata[:,3])
+  data["workclass"] = data[["workclass"]].replace(' ?', workclassmode)
+  data["education"] = data[["education"]].replace(' ?',  educationmode)
+  data["marital-status"] = data[["marital-status"]].replace(' ?', maritalmode)
+  data["occupation"] = data[["occupation"]].replace(' ?', occupationmode)
+  data["relationship"] = data[["relationship"]].replace(' ?',  relationshipmode)
+  data["race"] = data[["race"]].replace(' ?',  racemode)
+  data["sex"] = data[["sex"]].replace(' ?', sexmode)
 
-  cidata[:,5] = le5.transform(cidata[:,5])
 
-  cidata[:,6] = le6.transform(cidata[:,6])
+  cidata = data.as_matrix()
+  cidata[:,0] = le1.transform(cidata[:,0])
 
-  cidata[:,7] = le7.transform(cidata[:,7])
+  cidata[:,1] = le3.transform(cidata[:,1])
 
-  cidata[:,8] = le8.transform(cidata[:,8])
+  cidata[:,2] = le5.transform(cidata[:,2])
 
-  cidata[:,9] = le9.transform(cidata[:,9])
+  cidata[:,3] = le6.transform(cidata[:,3])
 
-  cidata[:,13] = le13.transform(cidata[:,13])
+  cidata[:,4] = le7.transform(cidata[:,4])
+
+  cidata[:,5] = le8.transform(cidata[:,5])
+
+  cidata[:,6] = le9.transform(cidata[:,6])
 
   from sklearn.externals import joblib
   filename = 'clf.sav'
